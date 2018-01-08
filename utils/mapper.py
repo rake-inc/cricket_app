aa15e9a3cede609ed794a9af13cbedf923404a51
Gautham <gautham.nambi@live.com>
2018-01-08 11:33:49 +0530

    url query type conversion

--- utils/mapper.py
+++ utils/mapper.py
@@ -6,6 +6,11 @@ def re_map_query_params(query_params_dict):
     try:
         for key in dict(query_params_dict).keys():
             result[key] = dict(query_params_dict).get(key).pop()
+            try:
+                result[key] = int(result[key])
+            except Exception as e:
+                logging.warning("MAPPER RE_MAP_QUERY_PARAMS EXCEPTION REACHED "
+                                "CANNOT BE CONVERTED TO INT: %s" % e)
     except Exception as e:
         logging.error("MAPPER RE_MAP_QUERY_PARAMS EXCEPTION REACHED %s" % e)
     return result
